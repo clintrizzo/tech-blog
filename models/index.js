76cfcfcc89999804a71df1be1b0b1292f57487ed
Clint <maxeva02@gmail.com>
2021-05-19 07:10:40 -0700

    went back to original code from tested file

--- models/index.js
+++ models/index.js
@@ -1,11 +1,30 @@
 const User = require('./User');
-const Comments = require('./Comments');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
-User.hasMany(Post, {as:"posts"});
-Post.belongsTo(User, {as:"user"});
+//create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
 
-
-module.exports = { User, Comments, Post };
+module.exports = {User, Post, Comment};
--- server.js
+++ server.js
@@ -1,42 +1,46 @@
-const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+
 const helpers = require('./utils/helpers');
 
-//import sequelize 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'Secret Secret Super',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+  secret: 'Secret Secret Super',
+  cookie: {
+        // Session will automatically expire in 10 minutes
+        expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
 };
 
 app.use(session(sess));
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(routes);
 
-sequelize.sync({force: false }).then(() => {
-    app.listen(PORT, () => {
-        console.log('App listening on port 3001!');
-    });
-})
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
